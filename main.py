b7ac430e85604b2bb162be55024584eefa6c2014
sovanedev <88293854+sovanedev@users.noreply.github.com>
2023-11-18 00:47:08 +0400

    Update main.py

--- main.py
+++ main.py
@@ -29,12 +29,12 @@ json_file_path = 'C:/Users/sovanedev/Desktop/e320tgbot/agreed_users.json'
 API_TOKEN = "6597665670:AAEJAwxR08H32eZCqJqrQqKyiO8H-XLICsc"
 DATABASE_URL = 'C:/Users/sovanedev/Desktop/e320tgbot/db.db'
 
-'''home_path = '/home/debian/tge320'
+home_path = '/home/debian/tge320'
 json_file_path = os.path.join(home_path, 'agreed_users.json')
 DATABASE_URL = os.path.join(home_path, 'db.db')
 
 os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
-os.makedirs(os.path.dirname(DATABASE_URL), exist_ok=True)'''
+os.makedirs(os.path.dirname(DATABASE_URL), exist_ok=True)
 
 #e320_id = -1002047946378 #test
 e320_id = -1001550842546 #osnova
